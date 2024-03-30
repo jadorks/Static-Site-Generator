29d83a7c97abd8ded092264353ac268aeef33ddb
jadorks <adorkorj@gmail.com>
2024-03-30 13:50:33 +0000

    Create TextNode class

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
-print("Hello world!")
+from textnode import TextNode
+
+
+def main():
+    text_node = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    print(text_node)
+
+main()
