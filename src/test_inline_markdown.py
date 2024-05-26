c74f2669a50876e38081402a3af02d289e539a60
jadorks <adorkorj@gmail.com>
2024-05-26 12:06:42 +0000

    Implement text to textnodes

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -5,6 +5,7 @@ from inline_markdown import (
     split_nodes_link,
     extract_markdown_images,
     extract_markdown_links,
+    text_to_textnodes,
 )
 
 from textnode import (
@@ -14,7 +15,7 @@ from textnode import (
     text_type_italic,
     text_type_code,
     text_type_image,
-    text_type_link
+    text_type_link,
 )
 
 
@@ -94,16 +95,13 @@ class TestInlineMarkdown(unittest.TestCase):
         self.assertListEqual(
             [
                 TextNode("This is text with an ", text_type_text),
-                TextNode("image", text_type_image, "https://i.imgur.com/zjjcJKZ.png")
+                TextNode("image", text_type_image, "https://i.imgur.com/zjjcJKZ.png"),
             ],
-            new_nodes
+            new_nodes,
         )
 
     def test_split_image_single(self):
-        node = TextNode(
-            "![image](https://www.example.com/image.png)",
-            text_type_text
-        )
+        node = TextNode("![image](https://www.example.com/image.png)", text_type_text)
         new_nodes = split_nodes_image([node])
         self.assertListEqual(
             [
@@ -145,6 +143,30 @@ class TestInlineMarkdown(unittest.TestCase):
                 TextNode(" with text that follows", text_type_text),
             ],
             new_nodes,
+        )
+
+    def test_text_to_textnodes(self):
+        text = "This is **text** with an *italic* word and a `code block` and an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and a [link](https://boot.dev)"
+
+        nodes = text_to_textnodes(text)
+
+        self.assertListEqual(
+            [
+                TextNode("This is ", text_type_text),
+                TextNode("text", text_type_bold),
+                TextNode(" with an ", text_type_text),
+                TextNode("italic", text_type_italic),
+                TextNode(" word and a ", text_type_text),
+                TextNode("code block", text_type_code),
+                TextNode(" and an ", text_type_text),
+                TextNode(
+                    "image",
+                    text_type_image,
+                    "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png",
+                ),
+                TextNode(" and a ", text_type_text),
+                TextNode("link", text_type_link, "https://boot.dev"),
+            ], nodes
         )
 
 
