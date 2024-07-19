39051549f957dfaf00b42627af8f582251787ff3
jadorks <adorkorj@gmail.com>
2024-07-19 23:14:44 +0000

    Implement block to block type

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,10 @@
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_unordered_list = "unordered_list"
+block_type_ordered_list = "ordered_list"
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     res = []
@@ -8,4 +15,38 @@ def markdown_to_blocks(markdown):
 
     return res
 
+def block_to_block_type(block: str):
+    lines = block.split("\n")
 
+    if block.startswith("# ") or block.startswith("## ") or block.startswith("### ") or block.startswith("#### ") or block.startswith("##### ") or block.startswith("###### "):
+        return block_type_heading
+    
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+        return block_type_code
+
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return block_type_paragraph
+        return block_type_quote
+    
+    if block.startswith("* "):
+        for line in lines:
+            if not line.startswith("* "):
+                return block_type_paragraph
+        return block_type_unordered_list
+    
+    if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return block_type_paragraph
+        return block_type_unordered_list
+    
+    if block.startswith("1. "):
+        for i in range(len(lines)):
+            line = lines[i]
+            if not line.startswith(f"{i+1}. "):
+                return block_type_paragraph
+        return block_type_ordered_list
+    
+    return block_type_paragraph
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,5 +1,14 @@
 import unittest
-from block_markdown import markdown_to_blocks
+from block_markdown import (
+    markdown_to_blocks,
+    block_to_block_type,
+    block_type_paragraph,
+    block_type_heading,
+    block_type_code,
+    block_type_ordered_list,
+    block_type_quote,
+    block_type_unordered_list,
+)
 
 
 class TestBlockMarkdown(unittest.TestCase):
@@ -24,7 +33,71 @@ class TestBlockMarkdown(unittest.TestCase):
 
         self.assertEqual(len(blocks), 0)
 
-        
+    def test_heading_type(self):
+        block = "# Hello this is a heading"
+        block_type = block_to_block_type(block)
+
+        self.assertEqual(block_type, block_type_heading)
+
+    def test_paragraph_type(self):
+        block = "Hello I am a paragraph"
+        block_type = block_to_block_type(block)
+
+        self.assertEqual(block_type, block_type_paragraph)
+
+    def test_code_type(self):
+        block = "```\nprint()\n```"
+        block_type = block_to_block_type(block)
+
+        self.assertEqual(block_type, block_type_code)
+
+    def test_ordered_list(self):
+        block = "1. Hello\n2. I am\n3. A list"
+        block_type = block_to_block_type(block)
+
+        self.assertEqual(block_type, block_type_ordered_list)
+
+    def test_invalid_ordered_list(self):
+        block = "1. Hello\nI am not valid"
+        block_type = block_to_block_type(block)
+
+        self.assertEqual(block_type, block_type_paragraph)
+
+    def test_unordered_list_asterisk(self):
+        block = "* Hello\n* I do not like order"
+        block_type = block_to_block_type(block)
+
+        self.assertEqual(block_type, block_type_unordered_list)
+
+    def test_unordered_list_dash(self):
+        block = "- Hello\n- I do not like order"
+        block_type = block_to_block_type(block)
+
+        self.assertEqual(block_type, block_type_unordered_list)
+
+    def test_invalid_unordered_list_asterisk(self):
+        block = "* Hello\nI am not valid"
+        block_type = block_to_block_type(block)
+
+        self.assertEqual(block_type, block_type_paragraph)
+
+    def test_invalid_unordered_list_dash(self):
+        block = "- Hello\nI am not valid"
+        block_type = block_to_block_type(block)
+
+        self.assertEqual(block_type, block_type_paragraph)
+
+    def test_quote(self):
+        block = "> Hello\n> A wise man once said\n> You are a quote"
+        block_type = block_to_block_type(block)
+
+        self.assertEqual(block_type, block_type_quote)
+
+    def test_invalid_quote(self):
+        block = "> Hello\nA wise man once said\nYou are not a quote"
+        block_type = block_to_block_type(block)
+
+        self.assertEqual(block_type, block_type_paragraph)
 
 
 if __name__ == "__main__":
