873b3b8dd1ac60a06032de6bbad9a69af67c5455
jadorks <adorkorj@gmail.com>
2024-07-20 01:50:20 +0000

    Implement public generator

--- src/main.py
+++ src/main.py
@@ -1,9 +1,30 @@
-from textnode import TextNode, text_type_bold
+import os
+import shutil
 
+def copy_source_to_destination(source, destination):
+    if not os.path.exists(source):
+        raise ValueError("Source directory does not exist")
+    
+    if not os.path.exists(destination):
+        os.mkdir(destination)
+    else:
+        shutil.rmtree(destination)
+        os.mkdir(destination)
+
+    static_files = os.listdir(source)
+
+    for entry in static_files:
+        path = os.path.join(source, entry)
+
+        if os.path.isfile(path):
+            shutil.copy(path, destination)
+        else:
+            dst = os.path.join(destination, entry)
+            copy_source_to_destination(path, dst)
+    
 
 def main():
-    text_node = TextNode("This is a text node", text_type_bold, "https://www.boot.dev")
-    print(text_node)
+    copy_source_to_destination('static', 'public')
 
 
 main()
